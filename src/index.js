80cc05a8a428d0f68069143aae31418d14e99ab3
Teng-hao Chang <zhusee2@gmail.com>
2017-06-30 12:30:13 +0800

    Widget should only show on mobile devices (except iPad)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+import MobileDetect from 'mobile-detect';
 import phoneIcon from './phone.svg';
 import './styles.css';
 
-const rootNode = document.createElement('div');
-rootNode.className = 'ichef-web-widget';
-rootNode.innerHTML = `
-    <a href="tel:0227600120" class="ichef-widget-btn" title="致電 iCHEF">
-        ${phoneIcon}
-    </a>
-`;
+function appendWidget() {
+    const rootNode = document.createElement('div');
+    rootNode.className = 'ichef-web-widget';
+    rootNode.innerHTML = `
+        <a href="tel:0227600120" class="ichef-widget-btn" title="致電 iCHEF">
+            ${phoneIcon}
+        </a>
+    `;
 
-document.body.appendChild(rootNode);
+    document.body.appendChild(rootNode);
+}
+
+const detector = new MobileDetect(navigator.userAgent);
+
+// Hide widget from iPad and desktop browsers
+if (detector.mobile() && !detector.is('iPad')) {
+    appendWidget();
+}
